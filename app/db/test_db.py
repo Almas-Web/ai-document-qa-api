6ed657701b8a9d10beef67521c10b6c3f1b288d4
Almas Hossen <almashossen028@gmail.com>
2026-09-10 13:52:31 +0600

    test: add API test suite

--- app/db/test_db.py
+++
@@ -1,8 +0,0 @@
-from app.db.database import engine
-
-try:
-    with engine.connect() as connection:
-        print("Database connection successful!")
-except Exception as error:
-    print("Database connection failed!")
-    print(error)
